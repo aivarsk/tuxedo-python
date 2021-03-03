fc0d647c4d20f0fed4f775c4018e9426500672aa
Aivars Kalvans <aivars.kalvans@gmail.com>
2021-03-03 21:04:57 +0000

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 import setuptools
 
 __name__ = 'tuxedo'
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 
 class get_pybind_include(object):
     """Helper class to determine the pybind11 include path
